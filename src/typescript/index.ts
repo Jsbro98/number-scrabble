4f781e92a4ef4c21c83dfd8004a137da41404eda
Jacob <96179296+Jsbro98@users.noreply.github.com>
2025-01-15 20:42:31 -0600

    edited todos, added an import

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -3,6 +3,7 @@ import {
   createAndAppendTiles,
   GameGrid,
   checkEquality,
+  GameGridFactory,
 } from './utils';
 import '../style.css';
 
@@ -14,6 +15,9 @@ todo list:
 TODO: add double click to return to original container
 TODO: change game to better denominations (5s, 2s, or 1s)
 TODO: test and possibly fix when equations touch
+  - rewrite equation handling to be less verbose.
+  - this should lead to fixing the combinations listed below
+    ** accumulate the whole string & split at the "=" signs
 
 1/4 - continue with equation combinations as listed above ^^
   ex: 1 + 1 = 4 - 2 = 3 - 1
@@ -26,56 +30,7 @@ TODO: test and possibly fix when equations touch
 
 function main(): void {
   // Main game grid
-  const gameGrid: GameGrid = {
-    rows: [
-      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-    ],
-
-    lastTenMoves: [],
-
-    getCell(row: number, index: number): string | null {
-      if (row < 0 || row > 14 || index < 0 || index > 14) return null;
-
-      const returnValue = this.rows[row][index];
-
-      if (returnValue === '') return null;
-
-      return returnValue;
-    },
-
-    setCell(row: number, index: number, value: string) {
-      this.rows[row][index] = value;
-      this.lastTenMoves.push({ position: [row, index], value });
-
-      if (this.lastTenMoves.length > 10) {
-        this.lastTenMoves.shift();
-      }
-    },
-
-    removeCell(row: number, index: number) {
-      this.rows[row][index] = '';
-      for (let move of this.lastTenMoves) {
-        if (move.position[0] === row && move.position[1] === index) {
-          const index = this.lastTenMoves.indexOf(move);
-          this.lastTenMoves.splice(index, 1);
-        }
-      }
-    },
-  };
+  const gameGrid: GameGrid = GameGridFactory();
 
   const numberTileContainer = document.querySelector('.number-tile-container');
   const operatorTileContainer = document.querySelector(
