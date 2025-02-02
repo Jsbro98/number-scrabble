1208e690c955566120e379becdbe8bf77d176f26
Jacob <96179296+Jsbro98@users.noreply.github.com>
2025-02-01 22:47:13 -0600

    implemented movableGridCell logic

--- src/typescript/movableGridCell.ts
+++ src/typescript/movableGridCell.ts
@@ -3,17 +3,27 @@ import { match } from 'ts-pattern';
 
 export class MovableGridCell implements TraversableCell {
   private grid: GameGrid;
+  private orignialRowValue: number;
+  private originalColumnValue: number;
+  public currentRow: number;
+  public currentColumn: number;
   public left: StringOrNull;
   public right: StringOrNull;
   public up: StringOrNull;
   public down: StringOrNull;
+  public current: StringOrNull;
 
   constructor(grid: GameGrid, row: number, column: number) {
     this.grid = grid;
+    this.currentRow = row;
+    this.currentColumn = column;
+    this.originalColumnValue = column;
+    this.orignialRowValue = row;
     this.left = grid.getCell(row, column - 1);
     this.right = grid.getCell(row, column + 1);
     this.up = grid.getCell(row - 1, column);
     this.down = grid.getCell(row + 1, column);
+    this.current = grid.getCell(row, column);
   }
 
   public moveCell(movement: Movement): void {
@@ -25,11 +35,37 @@ export class MovableGridCell implements TraversableCell {
       .exhaustive();
   }
 
-  private moveLeft() {}
+  public resetGridValues() {
+    this.currentColumn = this.originalColumnValue;
+    this.currentRow = this.orignialRowValue;
+    this.updateValues();
+  }
 
-  private moveRight() {}
+  private moveLeft() {
+    this.currentColumn -= 1;
+    this.updateValues();
+  }
 
-  private moveUp() {}
+  private moveRight() {
+    this.currentColumn += 1;
+    this.updateValues();
+  }
 
-  private moveDown() {}
+  private moveUp() {
+    this.currentRow -= 1;
+    this.updateValues();
+  }
+
+  private moveDown() {
+    this.currentRow += 1;
+    this.updateValues();
+  }
+
+  private updateValues() {
+    this.left = this.grid.getCell(this.currentRow, this.currentColumn - 1);
+    this.right = this.grid.getCell(this.currentRow, this.currentColumn + 1);
+    this.up = this.grid.getCell(this.currentRow - 1, this.currentColumn);
+    this.down = this.grid.getCell(this.currentRow + 1, this.currentColumn);
+    this.current = this.grid.getCell(this.currentRow, this.currentColumn);
+  }
 }
